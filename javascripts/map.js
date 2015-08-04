858dd88a946fdac8f3da08f07d65b704590be8fb
jgthibodeau <jgthibodeau@umass.edu>
2015-08-04 14:50:13 -0400

    working to fix regular map printing

--- javascripts/map.js
+++ javascripts/map.js
@@ -1555,28 +1555,32 @@ function printMap(){
 	var viewportwidth;
 	var viewportheight;
 	// the more standards compliant browsers (mozilla/netscape/opera/IE7) use window.innerWidth and window.innerHeight
-	if (typeof window.innerWidth != 'undefined'){
-		viewportwidth = window.innerWidth,
-		viewportheight = window.innerHeight
-	}
-	// IE6 in standards compliant mode (i.e. with a valid doctype as the first line in the document)
-	else if (typeof document.documentElement != 'undefined' && typeof document.documentElement.clientWidth != 'undefined' && document.documentElement.clientWidth != 0){
-		viewportwidth = document.documentElement.clientWidth,
-		viewportheight = document.documentElement.clientHeight
-	}
-	// older versions of IE
-	else{
-		viewportwidth = document.getElementsByTagName('body')[0].clientWidth,
-		viewportheight = document.getElementsByTagName('body')[0].clientHeight
-	}
+	// if (typeof window.innerWidth != 'undefined'){
+	// 	viewportwidth = window.innerWidth,
+	// 	viewportheight = window.innerHeight
+	// }
+	// // IE6 in standards compliant mode (i.e. with a valid doctype as the first line in the document)
+	// else if (typeof document.documentElement != 'undefined' && typeof document.documentElement.clientWidth != 'undefined' && document.documentElement.clientWidth != 0){
+	// 	viewportwidth = document.documentElement.clientWidth,
+	// 	viewportheight = document.documentElement.clientHeight
+	// }
+	// // older versions of IE
+	// else{
+	// 	viewportwidth = document.getElementsByTagName('body')[0].clientWidth,
+	// 	viewportheight = document.getElementsByTagName('body')[0].clientHeight
+	// }
 
-	$('#main-content').height(viewportheight);
-	$('#main-content').width(viewportwidth);
+	$('#main-content').height($('body').height());
+	$('#main-content').width($('body').width());
+	$('#map-canvas').height($('body').height());
+	$('#map-canvas').width($('body').width());
 
 	window.print();
 	
 	$('#main-content').height('');
 	$('#main-content').width('');
+	$('#main-content').height('');
+	$('#map-canvas').width('');
 }
 
 //print just the text directions
