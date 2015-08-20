d21737ded61b1fdd3ecb8802dffa303e2990ea74
jgthibodeau <jgthibodeau@umass.edu>
2015-08-20 15:50:30 -0400

    possible getElementsByClassName fix for IE

--- javascripts/map.js
+++ javascripts/map.js
@@ -729,7 +729,7 @@ function setInitialLayers(){
 	// $(displayOverlay.image_).find('.layer').hide();
 	// $(clickableOverlay.image_).find('.layer').hide();
 	$(getElementsByClassName(displayOverlay.image_, "layer")).hide();
-	$(getElementsByClassName(displayOverlay.image_, "layer")).hide();
+	$(getElementsByClassName(clickableOverlay.image_, "layer")).hide();
 
 	if(params["layers"] != undefined){
 		params["layers"].forEach(function(layer){
